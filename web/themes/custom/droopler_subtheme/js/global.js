006610a7c829c9d29b6b9bba15fe71b406386a73
Pawel Gorski <pawel.gorski@droptica.pl>
2021-04-19 01:39:37 +0200

    Maps & Discounts

--- web/themes/custom/droopler_subtheme/js/global.js
+++ web/themes/custom/droopler_subtheme/js/global.js
@@ -5,8 +5,38 @@
   Drupal.behaviors.droopler_subtheme = {
     attach: function (context, settings) {
 
-      // Insert your code here.
+      //todo aattached to views not global.
+      $('.view-id-discounts.view-display-id-attachment_1').once().append("<i class='bi bi-arrow-down-circle-fill iframe-resize'></i>");
 
+      $('.view-id-discounts .views-exposed-form').once().append("<span class='filter-resize'><i class=\"bi bi-arrows-collapse\"></i><i class='bi arrow-bar-down filter-resize'></i><i class=\"bi bi-filter\"></i> Filtry</span>");
+
+
+      $( ".iframe-resize" ).on( "click", function() {
+        let ifheight;
+        if($(this).hasClass('full')) {
+          $(this).removeClass('full');
+          ifheight = '30vh';
+        }
+        else {
+          ifheight = '90vh';
+          $(this).addClass('full');
+        }
+        $('.geofield-google-map').animate({height: ifheight}, 500);
+      });
+
+
+      $( ".filter-resize" ).on( "click", function() {
+        let filter;
+        if($('.views-exposed-form').hasClass('full')) {
+          $('.views-exposed-form').removeClass('full');
+          filter = '85%';
+        }
+        else {
+          filter = '0';
+          $('.views-exposed-form').addClass('full');
+        }
+        $('.views-exposed-form .form--inline').animate({height: filter}, 500);
+      });
     }
   };
 
